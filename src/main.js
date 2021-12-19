2b8f498c2773c5b34f2dd83d624322c92ed98ed9
Marcos Victor <marcosvictorsb@gmail.com>
2021-12-19 12:52:52 -0300

    clear the file main.js

--- src/main.js
+++
@@ -1,7 +0,0 @@
-let a = 3;
-let b = 2;
-let c = 'sfdfsdfsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd sfdfsdfsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd';
-
-a = b;
-b = c;
-c = a;
